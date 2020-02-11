3cfeab52ee68cddd1091e4fcdcadb5aed6916891
Anushka Menon <57836234+Anushka2501@users.noreply.github.com>
2020-02-12 02:40:54 +0530

    Add files via upload

---
+++ flux.py
@@ -0,0 +1,18 @@
+##Flux from absolute magnitude
+
+import math
+
+def absmag_to_appmag(M,D):
+    app_mag = M+5*math.log(D/10,10)         # m is apparent magnitude
+    return app_mag
+
+def appmag_to_flux(app_mag):
+    flux = 10**((app_mag-24)/-2.5)        # zp mag = 24  
+    return flux
+
+D = float(input("The effective radius in parsec: "))
+M = float(input("The absolute magnitude is: "))
+a = absmag_to_appmag(M,D)
+b = appmag_to_flux(a)
+print(a)
+print(b)
---
+++ velocitydispersion.py
@@ -0,0 +1,18 @@
+##Velocity Dispersion
+
+def luminosity(h,R):       
+    lum = ((10**10.2*h**-2)*(R/10**0.52*h**-1)**1.5)
+    return lum
+
+def vel_dispersion(L,h):
+    sigma = (((L/10**10.2*h**-2)**0.25)*10**2.197)
+    return sigma
+
+R = float(input("Enter the effective radius in kpc: "))
+h = float(0.72/0.7)           # h=h70 is reduced Hubbles constant
+lum = luminosity(h,R)           # In terms of solar luminosity
+sigma = vel_dispersion(lum,h)   # In kilometer/sec 
+print(lum)
+print(sigma)
+
+
