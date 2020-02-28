08325f375c56dbec7d58f7c0a220a2caca2a3abc
Anushka Menon <57836234+Anushka2501@users.noreply.github.com>
2020-02-28 11:49:57 +0530

    Add files via upload

---
+++ redshift.py
@@ -0,0 +1,11 @@
+from astropy.cosmology import FlatLambdaCDM
+import numpy as np
+from astropy import units as u
+cosmo = FlatLambdaCDM(H0=72, Om0=0.26)
+
+d_A = cosmo.angular_diameter_distance(z=1.0) 
+
+theta = 0.439*u.arcsec  #theta in arcsec = 0.439
+distance_pc = (theta * d_A).to(u.pc, u.dimensionless_angles()) 
+
+print(distance_pc)
